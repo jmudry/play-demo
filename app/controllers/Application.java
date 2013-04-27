c91554c64068edb794df71b8a2ce192bedd3603c
Jan Mudry <jairek@o2.pl>
2013-04-27 11:19:12 +0200

    dodane liczenie liczby pierwsej

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import classes.Lobuzy;
 import classes.ParaLiczb;
 import classes.Trojmian;
 import play.*;
@@ -16,12 +17,12 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
-		public static Result sayHello(String imie) {
+	public static Result sayHello(String imie) {
         return ok(index.render("Hello " + imie + "!"));
     }
 
 
-		public static Result compute() {
+	public static Result compute() {
   		JsonNode json = request().body().asJson();
             ObjectNode result = Json.newObject();
 			if(json == null) {
@@ -49,8 +50,130 @@ public class Application extends Controller {
                     }
 
                     return ok(result);
-				}
-			}
-}
+		        }
+	    }
+    }
+
+    public static Result cyfrokrad() {
+        JsonNode json = request().body().asJson();
+        ObjectNode return_result = Json.newObject();
+        Integer result, number;
+        if(json == null) {
+            return badRequest("Expecting Json data");
+        } else {
+            String numberString = json.findPath("number").getTextValue();
+
+            if(numberString == null) {
+                return badRequest("Missing parameters");
+            } else {
+                number = Integer.parseInt(numberString);
+                try {
+                    result = Lobuzy.cyfrokrad(number);
+                    return_result.put("result", result);
+
+                } catch (Exception e){
+                    return_result.put("ERROR", "Parsing Error");
+                }
+
+                return ok(return_result);
+            }
+        }
+    }
+
+    public static Result hultajchochla() {
+        JsonNode json = request().body().asJson();
+        ObjectNode return_result = Json.newObject();
+        Integer result, number;
+        if(json == null) {
+            return badRequest("Expecting Json data");
+        } else {
+            String numberString = json.findPath("number").getTextValue();
+
+            if(numberString == null) {
+                return badRequest("Missing parameters");
+            } else {
+                number = Integer.parseInt(numberString);
+                try {
+                    result = Lobuzy.hultajchochla(number);
+                    return_result.put("result", result);
+
+                } catch (Exception e){
+                    return_result.put("ERROR", "Parsing Error");
+                }
+
+                return ok(return_result);
+            }
+        }
+    }
+
+    public static Result nieksztaltek() {
+        JsonNode json = request().body().asJson();
+        ObjectNode return_result = Json.newObject();
+        Integer result, number;
+        if(json == null) {
+            return badRequest("Expecting Json data");
+        } else {
+            String numberString = json.findPath("number").getTextValue();
+
+            if(numberString == null) {
+                return badRequest("Missing parameters");
+            } else {
+                number = Integer.parseInt(numberString);
+                try {
+                    result = Lobuzy.nieksztaltek(number);
+                    return_result.put("result", result);
+
+                } catch (Exception e){
+                    return_result.put("ERROR", "Parsing Error");
+                }
+
+                return ok(return_result);
+            }
+        }
+    }
+
+    public static Result prime() {
+        JsonNode json = request().body().asJson();
+        ObjectNode return_result = Json.newObject();
+        Integer result, number;
+        int k=0;
+        int prime =0;
+        int i;
+        if(json == null) {
+            return badRequest("Expecting Json data");
+        } else {
+            String numberString = json.findPath("number").getTextValue();
+
+            if(numberString == null) {
+                return badRequest("Missing parameters");
+            } else {
+                number = Integer.parseInt(numberString);
+                try {
+                   for (i=0; i< number; i++) {
+                       if (isPrime(i)) {
+                        prime = i;
+                       }
+                   }
+                    return_result.put("result", prime);
+
+                } catch (Exception e){
+                    return_result.put("ERROR", "Parsing Error");
+                }
+
+                return ok(return_result);
+            }
+        }
+    }
+
+
+    public static boolean isPrime(int n) {
+        for(int i=2;i<n;i++) {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+
+
 
 }
